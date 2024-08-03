de413fbdb856a64c322bf57b2119d9098a77d96f
IrynaShyrobokova <i.v.shyrobokova@gmail.com>
2024-08-03 13:12:10 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+﻿const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+});
